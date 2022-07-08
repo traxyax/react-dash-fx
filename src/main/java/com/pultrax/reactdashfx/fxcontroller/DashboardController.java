6333d6eda4003f1e244907367345cdc75b73a500
Aboubacar TRAORE <traxyax@gmail.com>
2022-07-08 18:01:59 +0000

    Update fxml and controller

--- src/main/java/com/pultrax/reactdashfx/fxcontroller/DashboardController.java
+++ src/main/java/com/pultrax/reactdashfx/fxcontroller/DashboardController.java
@@ -1,6 +1,15 @@
 package com.pultrax.reactdashfx.fxcontroller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.AreaChart;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.PieChart;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import org.springframework.stereotype.Component;
 
 import java.net.URL;
@@ -8,6 +17,60 @@ import java.util.ResourceBundle;
 
 @Component
 public class DashboardController implements Initializable {
+
+    @FXML
+    private TableColumn<?, ?> agentCodeCol;
+
+    @FXML
+    private TableColumn<?, ?> amountCol;
+
+    @FXML
+    private CheckBox areaCB;
+
+    @FXML
+    private AreaChart<?, ?> areaChart;
+
+    @FXML
+    private CheckBox barCB;
+
+    @FXML
+    private BarChart<?, ?> barChart;
+
+    @FXML
+    private TableColumn<?, ?> dateCol;
+
+    @FXML
+    private CheckBox lineCB;
+
+    @FXML
+    private LineChart<?, ?> lineChart;
+
+    @FXML
+    private Label nbAgentL;
+
+    @FXML
+    private Label nbProductL;
+
+    @FXML
+    private CheckBox pieCB;
+
+    @FXML
+    private PieChart pieChart;
+
+    @FXML
+    private TableColumn<?, ?> productCodeCol;
+
+    @FXML
+    private TableColumn<?, ?> quant;
+
+    @FXML
+    private TableView<?> saleTable;
+
+    @FXML
+    private Label turnoverL;
+
+    @FXML
+    private TableColumn<?, ?> unitPriceCol;
 
     private static DashboardController instance;
 
@@ -25,6 +88,6 @@ public class DashboardController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        
+
     }
 }
