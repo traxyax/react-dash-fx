73482f2e3f92ec7f1c694087bc9e5c3819ea7f26
Aboubacar TRAORE <traxyax@gmail.com>
2022-07-08 16:02:23 +0000

    Initial setup

---
+++ src/main/java/com/pultrax/reactdashfx/ReactDashFxApplication.java
@@ -0,0 +1,13 @@
+package com.pultrax.reactdashfx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactDashFxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactDashFxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pultrax/reactdashfx/ReactDashFxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pultrax.reactdashfx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactDashFxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
